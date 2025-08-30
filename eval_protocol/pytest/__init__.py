a0b44a2c2ff504ff662218d608ebf5459db94535
Derek Xu <xzrderek@gmail.com>
2025-08-30 15:25:20 -0700

    Fix langgraph

--- eval_protocol/pytest/__init__.py
+++ eval_protocol/pytest/__init__.py
@@ -3,7 +3,6 @@ from .default_dataset_adapter import default_dataset_adapter
 from .default_mcp_gym_rollout_processor import MCPGymRolloutProcessor
 from .default_no_op_rollout_processor import NoOpRolloutProcessor
 from .default_single_turn_rollout_process import SingleTurnRolloutProcessor
-from .default_langchain_rollout_processor import LangGraphRolloutProcessor
 from .evaluation_test import evaluation_test
 from .exception_config import ExceptionHandlerConfig, BackoffConfig, get_default_exception_handler_config
 from .rollout_processor import RolloutProcessor
@@ -18,12 +17,20 @@ except ImportError:
     PYDANTIC_AI_AVAILABLE = False
     PydanticAgentRolloutProcessor = None
 
+# Conditional import for optional LangChain dependency
+try:
+    from .default_langchain_rollout_processor import LangGraphRolloutProcessor
+
+    LANGCHAIN_AVAILABLE = True
+except ImportError:
+    LANGCHAIN_AVAILABLE = False
+    LangGraphRolloutProcessor = None
+
 __all__ = [
     "AgentRolloutProcessor",
     "MCPGymRolloutProcessor",
     "RolloutProcessor",
     "SingleTurnRolloutProcessor",
-    "LangGraphRolloutProcessor",
     "NoOpRolloutProcessor",
     "default_dataset_adapter",
     "RolloutProcessorConfig",
@@ -36,3 +43,6 @@ __all__ = [
 # Only add to __all__ if available
 if PYDANTIC_AI_AVAILABLE:
     __all__.append("PydanticAgentRolloutProcessor")
+
+if LANGCHAIN_AVAILABLE:
+    __all__.append("LangGraphRolloutProcessor")
